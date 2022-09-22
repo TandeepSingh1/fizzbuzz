37356d599034a1ebe7e825e7caceb8c17c11568c
TandeepSingh1 <tn678108@dal.ca>
2022-09-22 16:36:33 -0300

    commit

---
+++ fizzbuzz-TandeepSingh1/fizzbuzztandeep.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+for i in range(1,101):
+    if(i%5==0 and i%3==0):
+        print("fizz")
+    elif(i%5==0):
+        print("buzz")
+    elif(i%3==0):
+        print("fizzbuzz")
